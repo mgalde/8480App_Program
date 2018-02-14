cb51216491aa7dfdf9c43abd897a78c9d71648ea
Michael <30883926+mgalde@users.noreply.github.com>
2018-02-13 19:09:07 -0600

    Initial commit Building application program

---
+++ app/src/main/java/com/apt_get_sudo/a8480app_program/MainActivity.java
@@ -0,0 +1,13 @@
+package com.apt_get_sudo.a8480app_program;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
